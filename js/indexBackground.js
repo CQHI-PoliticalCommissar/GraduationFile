5a3833d76afe735000d8e301e32f298d9a0a842c
CQHI-PoliticalCommissar <2796693830@qq.com>
2024-11-30 19:51:28 +0800

    Delete js directory

--- js/indexBackground.js
+++
@@ -1,8 +0,0 @@
-// This file is used to generate a random background color for the index page.
-document.addEventListener("DOMContentLoaded", function getRandomColor() {
-    const randomColor1 = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    const randomColor2 = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-
-    document.body.style.background = `linear-gradient(45deg, ${randomColor1}, ${randomColor2})`;
-    window.onload = getRandomColor;
-});
